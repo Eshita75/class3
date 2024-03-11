9ebd142a21cfb1b8d4756afe5b467c9c48d35f3c
Eshita75 <163004513+Eshita75@users.noreply.github.com>
2024-03-11 20:52:00 +0600

    Initial commit

---
+++ bin/class2.dart
@@ -0,0 +1,12 @@
+void main(){
+  print("hello world");
+
+  // int firstNumber  -- camel case
+  // int first_number -- snake case
+  // inr FIRSTNUMBER -- PASCALCASE
+
+  double cgpa = 3.86;
+  int gpa = 3;
+  int total = cgpa.toInt() + gpa;//typecasting
+  print(total);
+}
